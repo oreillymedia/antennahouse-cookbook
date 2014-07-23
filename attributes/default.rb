2b0591cded540cb22c25de1f040f4841f0be1cba
Sanders Kleinfeld <sanders@oreilly.com>
2014-07-23 12:09:22 -0400

    Updated recipes to v0.1.0, which supports multiple AH installs.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,1 @@
-default['antennahouse']['baseurl'] = "http://www.antenna.co.jp/XML/download/ahf62/~Linux/"
-default['antennahouse']['filename'] = "AHFormatterV62_64-6.2E-R1.x86_64.rpm.gz"
-default['antennahouse']['install_path'] = "/usr/AHFormatterV62_64" # this should be set to whatever install path AH will install to
+default['antennahouse']['installs'] = [{"baseurl" => "http://www.antenna.co.jp/XML/download/ahf62/~Linux/",  "filename" => "AHFormatterV62_64-6.2E-M2.x86_64.rpm.gz", "install_dir" => "/usr/AHFormatterV62_64-MR2" } ]
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,39 +1,65 @@
-url_file = "#{node["antennahouse"]["baseurl"]}/#{node["antennahouse"]["filename"]}"
-tmp_file = "/tmp/#{node["antennahouse"]["filename"]}"
-unpacked_file = tmp_file.gsub(".gz", "")
+node["antennahouse"]["installs"].each do |install|
 
-remote_file tmp_file do
-  owner     'root'
-  group     'root'
-  mode      '0644'
-  backup    false
-  source    url_file
-  not_if    { ::File.exists?(tmp_file) || ::File.exists?(unpacked_file) || ::File.directory?(node["antennahouse"]["install_path"]) }
-end
+  url_file = "#{install["baseurl"]}/#{install["filename"]}"
+  tmp_file = "/tmp/#{install["filename"]}"
+  tmp_install_dir = "/tmp/ahtemp"
+  unpacked_file = tmp_file.gsub(".gz", "")
+  deb_file = unpacked_file.gsub(/AHFormatterV(\d*)_64-(.*?).x86_64.rpm/, 'ahformatterv\1-64_\2_amd64.deb')
+  install_dir = install["install_dir"]
+  install_shell_script = "#{install_dir}/run.sh"
 
-execute 'antennahouse-unpack' do
-  command   "gzip -N -d /tmp/#{node["antennahouse"]["filename"]}"
-  only_if   { ::File.exists?(tmp_file) }
-  not_if    { ::File.exists?(unpacked_file) || ::File.directory?(node["antennahouse"]["install_path"]) }
-end
+  remote_file tmp_file do
+    owner     'root'
+    group     'root'
+    mode      '0644'
+    backup    false
+    source    url_file
+    not_if    { ::File.exists?(tmp_file) || ::File.exists?(unpacked_file) || ::File.directory?(install["install_dir"]) }
+  end
 
-package "alien" do
-  action [:install, :upgrade]
-end
+  execute 'antennahouse-unpack' do
+    command   "gzip -N -d /tmp/#{install["filename"]}"
+    only_if   { ::File.exists?(tmp_file) }
+    not_if    { ::File.exists?(unpacked_file) || ::File.directory?(install["install_dir"]) }
+  end
 
-execute 'antennahouse-unpack' do
-  command "alien -i -d -c #{unpacked_file}"
-  not_if  { ::File.exists?("#{node["antennahouse"]["install_path"]}/run.sh") }
-end
+  package "alien" do
+    action [:install, :upgrade]
+  end
 
-ruby_block "html.css to html.css.bak" do
-  block do
-    filename = "#{node["antennahouse"]["install_path"]}/etc/html.css"
-    if ::File.exists?(filename)
-      ::File.rename(
-        filename, 
-        "#{filename}.bak"
-      )
+  execute 'antennahouse-unpack-debian' do
+    command "alien -k #{unpacked_file}; mv #{File.basename(deb_file)} #{deb_file}"
+    not_if  { ::File.directory?(install["install_dir"]) }
+  end
+
+  execute 'dpkg-unpack' do
+    # unpack in a temp dir
+    command "dpkg-deb -x #{deb_file} #{tmp_install_dir}" 
+    not_if  { ::File.directory?(install["install_dir"]) }
+  end
+
+  execute 'ah-move' do
+    # move to the proper locale, and update the run.sh file as needed
+    command "mv #{tmp_install_dir}/usr/AH* #{install_dir}" 
+    not_if  { ::File.directory?(install["install_dir"]) }
+  end
+
+  execute 'update-shell-script' do
+    # Update home directory in shell script
+    command "perl -p -i -e \"s%\\@\\@HOME\\@\\@%#{install_dir}%g\" #{install_shell_script}"
+    not_if { !::File.exists?(install_shell_script) }
+  end
+
+  ruby_block "html.css to html.css.bak" do
+    block do
+      filename = "#{install["install_dir"]}/etc/html.css"
+      if ::File.exists?(filename)
+        ::File.rename(
+          filename, 
+          "#{filename}.bak"
+        )
+      end
     end
   end
+
 end
